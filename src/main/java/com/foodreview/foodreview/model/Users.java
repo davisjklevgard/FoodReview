f24a987df9ac3ef318d4999a375333ba72870a5d
davisjklevgard <48463222+davisjklevgard@users.noreply.github.com>
2023-06-01 11:44:42 -0500

    Intial commit for Food Review

---
+++ src/main/java/com/foodreview/foodreview/model/Users.java
@@ -0,0 +1,70 @@
+package com.foodreview.foodreview.model;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+@Entity
+public class Users implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, updatable = false)
+    // Variables
+    private Long id;
+    private String name;
+    private String username;
+    private String email;
+    private String image;
+    // private arrays for favorite cities and restaurants
+
+    public Users() {}
+
+    public Users(String name, String username, String email, String image) {
+        this.name = name;
+        this.username = username;
+        this.email = email;
+        this.image = image;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+}
---
+++ src/test/java/com/foodreview/foodreview/FoodReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodreview.foodreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodReviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
